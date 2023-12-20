a88450985dbd1097c7e1eeffff228d41d366117c
V3 <122877337@qq.com>
2023-12-20 11:54:17 +0800

    程序入口

--- main.py
+++ main.py
@@ -1,29 +1,15 @@
 import uvicorn
 from fastapi import FastAPI
-from pymongo import MongoClient
+import routes.default
 
 app = FastAPI(
-    debug=True,
+    debug=False,
     version='1.0.0',
     title='EVE',
     description='也不知道要做什么，写着玩吧',
 )
 
-
-@app.on_event("startup")
-def startup_db_client():
-    app.mongodb_client = MongoClient()
-    app.db = app.mongodb_client['eve']
-
-
-@app.on_event("shutdown")
-def shutdown_db_client():
-    app.mongodb_client.close()
-
-
-@app.get(path='/', name='默认首页')
-async def default():
-    return {}
+app.include_router(routes.default.router)
 
 if __name__ == '__main__':
     uvicorn.run('main:app', reload=True)
